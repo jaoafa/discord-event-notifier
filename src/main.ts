e6752b037115d491348b3b1b3be0b69cee666262
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2021-12-31 13:46:37 +0900

    feat: init

---
+++ src/main.ts
@@ -0,0 +1,172 @@
+import { SlashCommandBuilder } from '@discordjs/builders'
+import config from 'config'
+import { ChannelType } from 'discord-api-types/v9'
+import {
+  Client,
+  Intents,
+  MessageEmbed,
+  Permissions,
+  TextChannel,
+} from 'discord.js'
+import {
+  formatDate,
+  getNotifyChannel,
+  isRegistered,
+  register,
+  unregister,
+} from './utlis'
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_SCHEDULED_EVENTS,
+    Intents.FLAGS.GUILD_MEMBERS,
+  ],
+})
+
+client.on('ready', async () => {
+  console.log(`ready: ${client.user?.tag}`)
+
+  for (const guild of client.guilds.cache.values()) {
+    try {
+      await guild.commands.create(
+        new SlashCommandBuilder()
+          .setName('event-notifier-register')
+          .setDescription('Register the guild')
+          .addChannelOption((option) =>
+            option
+              .setName('channel')
+              .setDescription('The channel to send the message to')
+              .addChannelType(ChannelType.GuildText)
+              .setRequired(true)
+          )
+          .toJSON()
+      )
+      await guild.commands.create({
+        name: 'event-notifier-unregister',
+        description: 'Unregister this guild',
+      })
+      console.log(`registered: ${guild.name} (${guild.id})`)
+    } catch (e) {
+      console.error(`Couldn't create commands: ${guild.name} (${guild.id})`)
+    }
+  }
+})
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return
+  const _guild = interaction.guild
+  if (!_guild) return
+  const guild = await _guild.fetch()
+  console.log(`interactionCreate: ${guild.name} (${guild.id})`)
+  const member = interaction.member
+  if (!member) return
+  if (
+    !(member.permissions as unknown as Permissions).has(
+      Permissions.FLAGS.ADMINISTRATOR
+    )
+  ) {
+    await interaction.reply(
+      ':x: You need to be an administrator to use this command.'
+    )
+    return
+  }
+  if (interaction.commandName === 'event-notifier-register') {
+    await interaction.reply(':pencil: Event notifier registering...')
+    const channel = interaction.options.getChannel('channel') as TextChannel
+    const registered = await register(guild, channel)
+    if (!registered) {
+      await interaction.editReply(':x: Event notifier is already registered.')
+      return
+    }
+    await interaction.editReply(
+      `:white_check_mark: Event notifier registered. Notify channel: <#${channel.id}>`
+    )
+  }
+
+  if (interaction.commandName === 'event-notifier-unregister') {
+    await interaction.reply(':pencil: Event notifier unregistering...')
+    const registered = await unregister(guild)
+    if (!registered) {
+      await interaction.editReply(':x: Event notifier is not registered.')
+      return
+    }
+    await interaction.editReply(
+      ':white_check_mark: Event notifier unregistered.'
+    )
+  }
+})
+
+client.on('guildScheduledEventCreate', async (event) => {
+  console.log(`Scheduled event ${event.id} created`)
+  const guild = event.guild
+  if (!guild) return
+  if (!isRegistered(guild)) {
+    return
+  }
+  const channel = await getNotifyChannel(guild)
+  if (!channel) {
+    return
+  }
+  const embed = new MessageEmbed()
+    .setTitle(`イベントが追加されました！`)
+    .setURL(event.url)
+    .addField('イベントタイトル', event.name)
+    .setColor('#00ff00')
+  if (event.scheduledStartTimestamp) {
+    embed.addField(
+      'イベント日時',
+      formatDate(event.scheduledStartAt, 'yyyy/MM/dd HH:mm:ss')
+    )
+  }
+  if (event.description) {
+    embed.addField('イベント説明', event.description)
+  }
+  if (event.creator) {
+    embed.addField('イベント作成者', `<@${event.creator.id}>`)
+  }
+  channel.send({
+    embeds: [embed],
+  })
+})
+
+client.on('guildScheduledEventUpdate', async (oldEvent, newEvent) => {
+  if (oldEvent == null || newEvent == null) return
+  const guild = newEvent.guild
+  if (guild == null) return
+  if (!isRegistered(guild)) {
+    return
+  }
+  console.log(`Scheduled event ${newEvent.id} updated`)
+  if (oldEvent.status !== 'SCHEDULED' || newEvent.status !== 'ACTIVE') {
+    return
+  }
+  const subscripbers = await newEvent.fetchSubscribers({
+    limit: 100,
+  })
+  const mentions = subscripbers.map((s) => `<@${s.user.id}>`).join(' ')
+
+  const channel = await getNotifyChannel(guild)
+  if (!channel) {
+    return
+  }
+  const embed = new MessageEmbed()
+    .setTitle(`イベントが始まりました！`)
+    .setURL(newEvent.url)
+    .addField('イベントタイトル', newEvent.name)
+    .setColor('#00ff00')
+  if (newEvent.description) {
+    embed.addField('イベント説明', newEvent.description)
+  }
+  if (newEvent.creator) {
+    embed.addField('イベント作成者', `<@${newEvent.creator.id}>`)
+  }
+  channel.send({
+    content: mentions,
+    embeds: [embed],
+  })
+})
+
+client
+  .login(config.get('discordToken'))
+  .then(() => console.log('Login Successful.'))
---
+++ src/utlis.ts
@@ -0,0 +1,66 @@
+import config from 'config'
+import { Guild, TextChannel } from 'discord.js'
+import fs from 'fs'
+
+const SERVERS_FILE = config.get<string>('serversFile')
+
+export async function register(guild: Guild, channel: TextChannel) {
+  if (!fs.existsSync(SERVERS_FILE)) {
+    fs.writeFileSync(SERVERS_FILE, JSON.stringify({}))
+  }
+  const servers: { [key: string]: string } = JSON.parse(
+    fs.readFileSync(SERVERS_FILE, 'utf8')
+  )
+  if (servers[guild.id]) return false
+  servers[guild.id] = channel.id
+  fs.writeFileSync(SERVERS_FILE, JSON.stringify(servers))
+  return true
+}
+
+export async function unregister(guild: Guild) {
+  if (!fs.existsSync(SERVERS_FILE)) {
+    fs.writeFileSync(SERVERS_FILE, JSON.stringify({}))
+  }
+  const servers: { [key: string]: string } = JSON.parse(
+    fs.readFileSync(SERVERS_FILE, 'utf8')
+  )
+  if (!servers[guild.id]) return false
+  delete servers[guild.id]
+  fs.writeFileSync(SERVERS_FILE, JSON.stringify(servers))
+  return true
+}
+
+export async function getNotifyChannel(
+  guild: Guild
+): Promise<TextChannel | null> {
+  if (!fs.existsSync(SERVERS_FILE)) {
+    fs.writeFileSync(SERVERS_FILE, JSON.stringify({}))
+  }
+  const servers: { [key: string]: string } = JSON.parse(
+    fs.readFileSync(SERVERS_FILE, 'utf8')
+  )
+  if (!servers[guild.id]) return null
+  const channel = await guild.channels.fetch(servers[guild.id])
+  if (!channel) return null
+  return channel as TextChannel
+}
+
+export async function isRegistered(guild: Guild) {
+  if (!fs.existsSync(SERVERS_FILE)) {
+    fs.writeFileSync(SERVERS_FILE, JSON.stringify([]))
+  }
+  const servers: { [key: string]: string } = JSON.parse(
+    fs.readFileSync(SERVERS_FILE, 'utf8')
+  )
+  return servers[guild.id] !== undefined
+}
+export function formatDate(date: Date, format: string): string {
+  format = format.replace(/yyyy/g, String(date.getFullYear()))
+  format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2))
+  format = format.replace(/dd/g, ('0' + date.getDate()).slice(-2))
+  format = format.replace(/HH/g, ('0' + date.getHours()).slice(-2))
+  format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2))
+  format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2))
+  format = format.replace(/SSS/g, ('00' + date.getMilliseconds()).slice(-3))
+  return format
+}
